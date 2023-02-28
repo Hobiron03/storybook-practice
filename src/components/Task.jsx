4abf2fe82d041273bb2e89d9a2d979bd3054cfa1
Yudai Kawakami <yudainepnep@icloud.com>
2023-02-28 20:17:38 +0900

    fix a11y

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function Task({
   task: { id, title, state },
@@ -9,7 +10,7 @@ export default function Task({
     <div className={`list-item ${state}`}>
       <label
         htmlFor="checked"
-        aria-label={`arachiveTask-${id}`}
+        aria-label={`archiveTask-${id}`}
         className="checkbox"
       >
         <input
@@ -19,7 +20,6 @@ export default function Task({
           id={`archiveTask-${id}`}
           checked={state === "TASK_ARCHIVED"}
         />
-        q
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
 
@@ -47,3 +47,19 @@ export default function Task({
     </div>
   );
 }
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
